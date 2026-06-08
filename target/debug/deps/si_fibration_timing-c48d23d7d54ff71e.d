63a418f5d2395e5871e4f7dc4a2ba4112dd25512
SuperInstance <superinstance@users.noreply.github.com>
2026-06-07 21:15:35 -0800

    Add .gitignore, remove target/ from tracking

--- target/debug/deps/si_fibration_timing-c48d23d7d54ff71e.d
+++
@@ -1,5 +0,0 @@
-/tmp/si-fibration-timing/target/debug/deps/si_fibration_timing-c48d23d7d54ff71e.d: src/lib.rs
-
-/tmp/si-fibration-timing/target/debug/deps/si_fibration_timing-c48d23d7d54ff71e: src/lib.rs
-
-src/lib.rs:
